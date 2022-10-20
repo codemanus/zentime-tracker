d748825bb28668dde05994670e66f774e3a3192c
codemanus <corkycodeman@proton.me>
2022-10-20 12:46:01 -0400

    Updates to the hoursTable

--- components/common/hoursTable.js
+++ components/common/hoursTable.js
@@ -4,33 +4,33 @@ export default function HoursTable(){
 
 
     return (
-        <div class="flex flex-col ">
-            <div class="overflow-x-auto sm:-mx-6 lg:-mx-8">
-                <div class="py-2 inline-block min-w-full sm:px-6 lg:px-8">
-                    <div class="overflow-hidden rounded">
-                        <table class="min-w-full">
-                            <thead class="bg-gray-100 border-b">
+        <div className="flex flex-col ">
+            <div className="overflow-x-auto sm:-mx-6 lg:-mx-8">
+                <div className="py-2 inline-block min-w-full sm:px-6 lg:px-8">
+                    <div className="overflow-hidden rounded">
+                        <table className="min-w-full">
+                            <thead className="bg-gray-100 border-b">
                                 <tr>
                                     <th
                                         scope="col"
-                                        class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                        className="text-md font-medium text-gray-900 px-6 py-4 text-left"
                                     >
                                         Category
                                     </th>
                                     <th
                                         scope="col"
-                                        class="text-sm font-medium text-gray-900 px-6 py-4 text-left"
+                                        className="text-md font-medium text-gray-900 px-6 py-4 text-left"
                                     >
                                         Hours
                                     </th>
                                 </tr>
                             </thead>
                             <tbody>
-                                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                    <td className="text-md text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                         Individual Counseling or Psychotherapy
                                     </td>
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                         <input
                                             type="text"
                                             name="hours"
@@ -40,12 +40,12 @@ export default function HoursTable(){
                                         />
                                     </td>
                                 </tr>
-                                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                    <td className="text-md text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                         Diagnosis & Treatment of Couples,
                                         Families, Children
                                     </td>
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                         <input
                                             type="text"
                                             name="hours"
@@ -55,11 +55,11 @@ export default function HoursTable(){
                                         />
                                     </td>
                                 </tr>
-                                <tr class="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                <tr className="bg-white border-b transition duration-300 ease-in-out hover:bg-gray-100">
+                                    <td className="text-md text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                         Group Therapy or Counseling
                                     </td>
-                                    <td class="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
+                                    <td className="text-sm text-gray-900 font-light px-6 py-4 whitespace-nowrap">
                                         <input
                                             type="text"
                                             name="hours"
